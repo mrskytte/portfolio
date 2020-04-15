2c1d5d0c6419504136ef801d8fef51b98ccd6274
Chris <mr.skytte@me.com>
2020-04-15 15:45:48 +0200

    current state - not working

--- main.js
+++ main.js
@@ -1,6 +1,60 @@
-import { listen, prefetch } from "quicklink";
+// import quicklink from "quicklink";
+import barba from "@barba/core";
+import gsap from "gsap";
 ("use strict");
 
 window.addEventListener("load", () => {
-  quicklink.listen();
+  console.log("ready");
+});
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+function pageTransition() {
+  let tl = gsap.timeline();
+
+  tl.to("ul.transition li", {
+    duration: 0.5,
+    scaleY: 1,
+    transformOrigin: "bottom left",
+    stagger: 0.2,
+  }).to("ul.transition li", {
+    duration: 0.5,
+    scaleY: 0,
+    transformOrigin: "bottom left",
+    stagger: 0.1,
+    delay: 0.1,
+  });
+}
+
+function contentAnimation() {
+  console.log("next page");
+}
+
+barba.init({
+  sync: true,
+
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+
+        pageTransition();
+        await delay(1500);
+        done();
+      },
+      async enter(data) {
+        contentAnimation();
+      },
+      async once(data) {
+        contentAnimation();
+      },
+    },
+  ],
 });
