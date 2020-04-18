76cf941c02575a7f75cabaaa5053a0c69ae5e924
Chris <mr.skytte@me.com>
2020-04-18 13:50:10 +0200

    responsiveness added to landing page

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import { listen, prefetch } from "quicklink";
-("use strict");
+"use strict";
 
-window.addEventListener("load", () => {
-  quicklink.listen();
-});
+window.addEventListener("load", init);
+
+function init() {
+  const links = document.querySelectorAll(".index-point a");
+
+  links.forEach((link) => {
+    link.addEventListener("touchstart", () => {
+      event.target.nextElementSibling.style.webkitAnimationPlayState = "paused";
+    });
+    link.addEventListener("touchend", () => {
+      event.target.nextElementSibling.style.webkitAnimationPlayState =
+        "running";
+    });
+  });
+}
